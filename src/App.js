0499c74191de9a0557363fef8fc79ada0341ca93
Mateo-Rey <jrey3darts@gmail.com>
2022-08-04 15:53:46 -0400

    calculator

--- src/App.js
+++ src/App.js
@@ -1,20 +1,46 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
+const options = [
+  {
+    label: "PST",
+    value: "PST",
+  },
+  {
+    label: "GMT",
+    value: "GMT",
+  },
+  {
+    label: "EST",
+    value: "EST",
+  },
+];
 
 function App() {
-  let count = 0
+  const [total, setTotal] = useState(0);
+  const [date, getDate] = useState(0);
+  let digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>My first react appdd</h1>
-        <p>You clicked the button {count} times</p>
-        <button onClick={() => {
-          console.log('I clicked the button')
-          count++;
-        }}>CLICK ME</button>
-      </header>
+    <>
+    <div className="table" id="calc">
+    {digits.map((digit) => {
+     return <button onClick={() => {
+      setTotal(total + digit)
+     }} key={digit}>{digit}</button>
+    })}
+    <button onClick={() => setTotal(0)}><b>reset</b></button>
+    <div className="total">Total: {total}</div>
     </div>
+      {/* <div className="table" id="table">
+        <div className="timezone-form">
+          <select>
+            {options.map((option) => (
+              <option value={option.value}>{option.label}</option>
+            ))}
+          </select>
+        </div>
+      </div> */}
+    </>
   );
 }
 export default App;
